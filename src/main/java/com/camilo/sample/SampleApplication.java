3f0cd084a5272f876159512046f00b2aabb0debc
CamiloTello002 <camilojtello5@gmail.com>
2024-08-03 15:30:52 -0500

    feat: add endpoint GET / for the sake of simplicity

--- src/main/java/com/camilo/sample/SampleApplication.java
+++ src/main/java/com/camilo/sample/SampleApplication.java
@@ -2,12 +2,22 @@ package com.camilo.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	@RestController
+	public static class HelloWorldController {
+		@GetMapping("/")
+		public String helloWorld() {
+			return "hello world";
+		}
 	}
 
 }
