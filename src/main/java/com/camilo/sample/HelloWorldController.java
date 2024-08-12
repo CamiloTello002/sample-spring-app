0c7cd7d7e2e8a38b1ba63a65e68d4334ec02746c
CamiloTello002 <camilojtello5@gmail.com>
2024-08-12 14:35:55 -0500

    feat(controller): Add HelloWorldController controller

--- src/main/java/com/camilo/sample/HelloWorldController.java
+++ src/main/java/com/camilo/sample/HelloWorldController.java
@@ -1,2 +1,11 @@
-package com.camilo.sample;public class HelloWorldController {
+package com.camilo.sample;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    public String helloWorld() {
+        return "Hello world!";
+    }
 }
