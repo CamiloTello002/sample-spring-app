a9bc4d2f55d50325ae3478959bd3e333cb45b910
CamiloTello002 <camilojtello5@gmail.com>
2024-08-18 14:34:26 -0500

    a very good commit
    In this commit, I just add comments about the rest controller.
    
    The purpose of this commit is to show the functionality when it comes to
    merge conflicts.

--- src/main/java/com/camilo/sample/HelloWorldController.java
+++ src/main/java/com/camilo/sample/HelloWorldController.java
@@ -3,6 +3,9 @@ package com.camilo.sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// this will be a comment that comes from the main branch
+// there are many things that you can do right here at this moment
+// and I like rest controllers because they do a pretty good job!
 @RestController
 public class HelloWorldController {
 
