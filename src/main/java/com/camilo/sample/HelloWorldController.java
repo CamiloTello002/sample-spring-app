dfe4562765e5aada7ff3a698dc7f9f420fbd2ea4
CamiloTello002 <camilojtello5@gmail.com>
2024-08-18 14:31:25 -0500

    this is a commit that will appear from this branch

--- src/main/java/com/camilo/sample/HelloWorldController.java
+++ src/main/java/com/camilo/sample/HelloWorldController.java
@@ -3,10 +3,10 @@ package com.camilo.sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// this thing that you can see here is a rest controller, which
+// means that it will handle http requests. I love this method
 @RestController
 public class HelloWorldController {
-
-    // this is a handler method
     @GetMapping(path={"/hello", "/what"})
     public String helloWorld() {
         return "Hello world!";
